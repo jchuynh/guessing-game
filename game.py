27b3c58baabc90a09bb583f6a0a406a76084c0a2
Your Name <you@example.com>
2020-01-07 16:26:47 -0800

    added try statements

--- game.py
+++ game.py
@@ -3,24 +3,41 @@
 # Put your code here
 import random
 
-name = input("""Hi! What is you're name?
-    > """)
+name = input("""Hi! What is you're name? > """)
 
 random_num = random.randint(0, 100)
 attempt = 1
 
 print(f"{name} I'm thinking of a number between 1 and 100. Guess my number.")
 
-user_input = input(">")
-user_input = int(user_input)
+
+while True:
+    try: 
+        user_input = int(input(">"))
+        break
+    except ValueError:
+        print("""Please enter an integer. > """)
+
 while True:
     if user_input != random_num:
         if user_input > random_num:
-            user_input = int(input("Your guess is too high, try again."))
-            attempt += 1
+            while True:
+                try:
+                    user_input = int(input("""Your guess is too high, try again.
+                    > """))
+                    attempt += 1
+                    break
+                except ValueError:
+                    print("""Please enter an integer. > """)
         elif user_input < random_num:
-            user_input = int(input("Your guess is too low, try again."))
-            attempt += 1
+            while True:
+                try:
+                    user_input = int(input("Your guess is too low, try again."))
+                    attempt += 1
+                    break
+                except ValueError:
+                    print("""Please enter an integer. > """)
+    
     else:
         print(f"Congatulations! you have found the number in {attempt} trys")
         break
